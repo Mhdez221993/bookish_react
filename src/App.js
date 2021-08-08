eb2203f6461f1c5e049a49ec60875e9e28274f7d
Moises <mhdezcoronado@gmail.com>
2021-08-08 16:13:10 +0200

    Move renderBooks method to a new file

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import BookList from './BookList';
 
 function App() {
   const books = [{ name: 'Refactoring' }, { name: 'Domain-driven design' }];
+
   return (
     <div className="App">
-     <Typography variant='h2' component='h2' data-test='heading'>
-      Bookish
-     </Typography>
-      <div data-test='book-list'>
-          { books.map(book => ( <div className='book-item'>
-           <h2 className='title'>{book.name}</h2> 
-          </div>)) }
-      </div>
-      { renderBooks(books) }
+      <Typography variant='h2' component='h2' data-test='heading'>
+        Bookish
+      </Typography>
+      <BookList books={ books } />
     </div>
   );
-}
-
-const renderBooks = (books) => {
-  return <div data-test='book-list'>
-    {
-      books.map(book => (<div className='book-item'>
-        <h2 className='title'>{book.name}</h2>
-      </div>))
-    }
-  </div>
 }
 
 export default App;
