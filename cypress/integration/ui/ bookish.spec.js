03f202d62fa30d14d73061f4065c15c3ad4d6a84
Moises <mhdezcoronado@gmail.com>
2021-08-08 15:11:37 +0200

    Add first end-to-end test

---
+++ cypress/integration/ui/ bookish.spec.js
@@ -0,0 +1,6 @@
+describe('Bookish application', () => {
+  it('Visits the bookish', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('h2[data-test="heading"]').contains('Bookish')
+  })
+})
