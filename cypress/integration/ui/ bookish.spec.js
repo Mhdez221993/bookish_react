3e919e51244df07cbcc484793a74db339ee2da3c
Moises <mhdezcoronado@gmail.com>
2021-08-08 15:44:26 +0200

    Refactor & add renderBook method to apply DRY principle

--- cypress/integration/ui/ bookish.spec.js
+++ cypress/integration/ui/ bookish.spec.js
@@ -3,4 +3,10 @@ describe('Bookish application', () => {
     cy.visit('http://localhost:3000/');
     cy.get('h2[data-test="heading"]').contains('Bookish')
   })
+
+  it('Shows a book list', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('div[data-test="book-list"]').should('exist');
+    cy.get('div.book-item').should('have.length', 4);
+  })
 })
